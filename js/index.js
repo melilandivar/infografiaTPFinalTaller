3969ede9c0dc65d7e433c33d953d1cedbb17c8aa
lukhoury <lucreciakhoury@gmail.com>
2021-11-06 14:23:05 -0300

    barra interactiva programada

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const controller = document.querySelector('input[type=range]');
+ const controller = document.querySelector('input[type=range]');
 const radialProgress = document.querySelector('.RadialProgress');
 
 const setProgress = (progress) => {
@@ -11,4 +11,26 @@ const setProgress = (progress) => {
 setProgress(controller.value)
 controller.oninput = () => {
   setProgress(controller.value)
+}
+
+
+
+
+var values = [1,3,5,10,20,50,100];    //values to step to
+
+var input = document.getElementById('input'),
+   output = document.getElementById('output');
+
+input.oninput = function(){
+    output.innerHTML = values[this.value];
+};
+input.oninput(); //set default value 
+
+function toggleSteps(element) {
+  var minutes = parseInt(element.value);
+  if (minutes > 10) {
+      element.step = 10;
+  } else {
+      element.step = 1;
+  }
 }
